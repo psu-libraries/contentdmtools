2829b37a17bd0ee8fd14d38a5af6782d0d6a9a57
Nathan Tallman <nathan@blacktallman.com>
2019-05-11 17:51:11 -0400

    Expand powershell aliases.

--- batchEdit/batchEdit.ps1
+++ batchEdit/batchEdit.ps1
@@ -1,7 +1,7 @@
 # Powershell script to process metadata changes in a single data table to a list of per-record field changes for CONTENTdm Catcher, via Pitcher. https://github.com/little9/pitcher
 # Nathan Tallman, August 2018
 
-# Read in the metadata changes with -csv path, pass the collection allias with -alias alias. 
+# Read in the metadata changes with -csv path, pass the collection allias with -alias alias.
 param (
     [string]$csv = "metadata.csv",
     [Parameter(Mandatory=$true)][string]$alias = $( Read-Host "Input the collection alias, please." )
@@ -25,18 +25,18 @@ Write-Output "cdmnumber,field,value,collection,action" | Out-File -FilePath tmp.
 
 # Find the changes and write out the row
 ForEach ($record in $metadata) {
-  Foreach ($header in $headers) {      
+  Foreach ($header in $headers) {
     Foreach ($data in $record.$header) {
       If ($data) {
         Write-Output ($record.cdmnumber + ","  + "$header"+ ',"'  + "$data"  +  '",' + "$alias" + "," + "edit")  | Out-File -FilePath tmp.csv -Append
-      } 
+      }
     }
   }
 }
 
 # Break the CSV's apart by field for per-record processing.
 ForEach ($header in $headers) {
- Import-Csv tmp.csv | Sort-Object * -Unique | Where {$_.field -eq "$header"} | Where {$_.field -ne "cdmnumber"} | Export-Csv ("metadata_edited_" + $header + ".csv") -NoTypeInformation
+ Import-Csv tmp.csv | Sort-Object * -Unique | Where-Object {$_.field -eq "$header"} | Where-Object {$_.field -ne "cdmnumber"} | Export-Csv ("metadata_edited_" + $header + ".csv") -NoTypeInformation
 }
 
 Remove-Item tmp.csv
@@ -46,7 +46,7 @@ Write-Output "$(Get-Timestamp) Metadata changes have now been broken up by field
 
 If(!(Test-Path logs)) {New-Item -ItemType Directory -Path logs | Out-Null }
 
-Get-ChildItem -Filter metadata_edited_* | ForEach {
+Get-ChildItem -Filter metadata_edited_* | ForEach-Object {
   pitcher --csv $_.FullName --settings settings.yml
   Read-Host -Prompt "Press any key and ENTER to continue when $_ been fully indexed or the items unlocked. Unless this was the last CSV, the next one will not pitch until $_ is unlocked or indexed. You can also press CTRL+C to quit."
 }
