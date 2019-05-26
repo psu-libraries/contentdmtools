a040bee3d29bd7fc01f066a986004513740d06f6
Nathan Tallman <nathan@blacktallman.com>
2019-05-25 21:54:31 -0400

    Update UI and tweak bachLoad

--- batchLoad/batchLoadCreate.ps1
+++ batchLoad/batchLoadCreate.ps1
@@ -1,24 +1,29 @@
-# batchLoadCreate.ps1 
+# batchLoadCreate.ps1
 # Nathan Tallman, Created in August 2018, Updated in November 2018
 # https://git.psu.edu/digipres/contentdm/edit/master/batchLoad
 
 # Setup the switches
 [cmdletbinding()]
-Param([switch]$abbyy, [switch]$im, [switch]$deltif)
+Param(
+  [switch]$abbyy,
+  [switch]$im,
+  [switch]$deltif,
+  [string]$source = $(Throw "Use -source to specify a path for the batch.")
+)
 
 # Setup timestamps and global variables
 function Get-TimeStamp { return "[{0:yyyy-MM-dd} {0:HH:mm:ss}]" -f (Get-Date) }
 $batch = $PSScriptRoot | Split-Path -Leaf
-$log = ($batch + "_cdmPrep_log.txt")
+$log = ("$source\$batch" + "_cdmPrep_log.txt")
 
 # Read in the metadata data.
-$metadata = Import-Csv -Delimiter "`t" -Path metadata.txt
+$metadata = Import-Csv -Path $source\metadata.csv
 
 # Find the directories (objects) and export the metadata for each one.
 $directories = $metadata | Group-Object -AsHashTable -AsString -Property Directory
 ForEach ($directory in $directories.keys)
 {
-  $directories.$directory |Export-Csv -Delimiter "`t" -Path $directory\$directory.txt -NoTypeInformation  | Tee-Object -file $log
+  $directories.$directory |Export-Csv -Delimiter "`t" -Path $source\$directory\$directory.txt -NoTypeInformation  | Tee-Object -file $log
 }
 Write-Output "$(Get-Timestamp) Metadata has been broken up into the the CONTENTdm Compound Object Directory Structure." | Tee-Object -file $log
 
@@ -26,31 +31,30 @@ Write-Output "$(Get-Timestamp) Metadata has been broken up into the the CONTENTd
 if($abbyy)
 {
   $count_staged = 0
-  $count_processed = 0
   $user = $env:USERNAME
   $abbyyStage = "O:\pcd\contentdm_withText\staging\$user\$batch"
   $abbyyInput = "O:\pcd\contentdm_withText\input\"
   $abbyyOutput = "O:\pcd\contentdm_withText\output\$batch"
 
   Write-Output "$(Get-Timestamp) Sending TIFs to ABBYY for conversion." | Tee-Object -file $log -Append
-  Write-Output "$(Get-Timestamp) Starting to copy..." | Tee-Object -file $log -Append 
+  Write-Output "$(Get-Timestamp) Starting to copy..." | Tee-Object -file $log -Append
   if(!(Test-Path $abbyyStage)) {New-Item -ItemType Directory -Path $abbyyStage | Out-Null }
-  robocopy . $abbyyStage *.tif* /S | Out-Null  
+  robocopy . $abbyyStage $source\*.tif* /S | Out-Null
   Copy-Item -Path $abbyyStage -Destination $abbyyInput -Recurse -Force | Out-Null
   Start-Sleep 30
   Remove-Item -Path $abbyyStage -Recurse -Force | Out-Null
   Write-Output "$(Get-Timestamp) Copy finished, ABBYY starting..." | Tee-Object -file $log -Append
-  
+
   DO {
-    $count_staged = Get-ChildItem -Path $PSScriptRoot -Include *.tif* -Recurse -File -Force | Measure-Object | %{$_.Count}
-    $count_txt = Get-ChildItem -Path "$abbyyOutput" -Include *.txt -Recurse -File -Force | Measure-Object | %{$_.Count}
-    $count_jp2 = Get-ChildItem -Path "$abbyyOutput" -Include *.jp2 -Recurse -File -Force | Measure-Object | %{$_.Count}
+    $count_staged = Get-ChildItem -Path $source -Include *.tif* -Recurse -File -Force | Measure-Object | ForEach-Object {$_.Count}
+    $count_txt = Get-ChildItem -Path "$abbyyOutput" -Include *.txt -Recurse -File -Force | Measure-Object | ForEach-Object {$_.Count}
+    $count_jp2 = Get-ChildItem -Path "$abbyyOutput" -Include *.jp2 -Recurse -File -Force | Measure-Object | ForEach-Object {$_.Count}
     Write-Output "  TIF count = $count_staged; TXT count = $count_txt; JP2 count = $count_jp2" | Tee-Object -file $log -Append
     Start-Sleep 30
   } Until (($count_staged -eq $count_txt) -and ($count_staged -eq $count_jp2))
 
    # Move the transcripts and jpeg2000s to the batch directory.
-   Move-Item -Path "$abbyyOutput\*" -Destination $PSScriptRoot  | Tee-Object -file $log
+   Move-Item -Path "$abbyyOutput\*" -Destination $source  | Tee-Object -file $log
    Start-Sleep 30
    Remove-Item -Path "$abbyyOutput" | Out-Null
    Write-Output "$(Get-Timestamp) ABBYY is finished, transcripts and jpeg2000s are ready." | Tee-Object -file $log -Append
@@ -59,13 +63,13 @@ if($abbyy)
 # Find the tif files and convert them to jp2.
 if($im){
   Write-Output "$(Get-Timestamp) Starting TIF to JP2 conversion..." | Tee-Object -file $log -Append
-  Get-ChildItem *.tif* -Recurse | ForEach { magick.exe convert -quiet ($_.FullName + "[0]") "$($_.FullName -Replace "tif", "jp2")" }  | Tee-Object -file $log
+  Get-ChildItem -Path $source -Include *.tif* -Recurse | ForEach-Object { magick.exe convert -quiet ($_.FullName + "[0]") "$($_.FullName -Replace "tif", "jp2")" } | Tee-Object -file $log
   Write-Output "$(Get-Timestamp) TIFs converted to JP2." | Tee-Object -file $log -Append
 }
 
 # Find the tif files and delete them.
 if($deltif){
   Write-Output "$(Get-Timestamp) Deleting TIFs...." | Tee-Object -file $log -Append
-  Get-ChildItem *.tif* -Recurse | ForEach { Remove-Item -Path $_.FullName }  | Tee-Object -file $log
+  Get-ChildItem -Path $source -Include *.tif* -Recurse | ForEach-Object { Remove-Item -Path $_.FullName } | Tee-Object -file $log
   Write-Output "$(Get-Timestamp) TIFs deleted." | Tee-Object -file $log -Append
 }
--- contentdmTools.ps1
+++ contentdmTools.ps1
@@ -17,19 +17,28 @@ $createBatchButton.BackColor     = "#1e407c"
 $createBatchButton.text          = "Create Batch"
 $createBatchButton.width         = 120
 $createBatchButton.height        = 49
-$createBatchButton.location      = New-Object System.Drawing.Point(19,79)
+$createBatchButton.location      = New-Object System.Drawing.Point(24,103)
 $createBatchButton.Font          = 'Microsoft Sans Serif,10,style=Bold'
 $createBatchButton.ForeColor     = "#ffffff"
 
 $createBatchPanel                = New-Object system.Windows.Forms.Panel
-$createBatchPanel.height         = 152
-$createBatchPanel.width          = 169
-$createBatchPanel.location       = New-Object System.Drawing.Point(11,14)
+$createBatchPanel.height         = 180
+$createBatchPanel.width          = 180
+$createBatchPanel.location       = New-Object System.Drawing.Point(23,14)
 $createBatchPanel.BackColor     = "#e0e0eb"
+
+$batchPathButton                 = New-Object system.Windows.Forms.Button
+$batchPathButton.BackColor       = "#1e407c"
+$batchPathButton.text            = "Set Batch Path"
+$batchPathButton.width           = 130
+$batchPathButton.height          = 30
+$batchPathButton.location        = New-Object System.Drawing.Point(19,40)
+$batchPathButton.Font            = 'Microsoft Sans Serif,10,style=Bold'
+$batchPathButton.ForeColor       = "#ffffff"
 
 $processor                       = New-Object system.Windows.Forms.ComboBox
 $processor.text                  = "ABBYY"
-$processor.width                 = 129
+$processor.width                 = 130
 $processor.height                = 20
 @('ABBYY','ImageMagick') | ForEach-Object {[void] $processor.Items.Add($_)}
 $processor.location              = New-Object System.Drawing.Point(18,15)
@@ -40,13 +49,13 @@ $deleteTIFF.text                 = "Delete TIFFs"
 $deleteTIFF.AutoSize             = $false
 $deleteTIFF.width                = 77
 $deleteTIFF.height               = 20
-$deleteTIFF.location             = New-Object System.Drawing.Point(42,49)
+$deleteTIFF.location             = New-Object System.Drawing.Point(40,78)
 $deleteTIFF.Font                 = 'Microsoft Sans Serif,10'
 
 $batchEditPanel                  = New-Object system.Windows.Forms.Panel
-$batchEditPanel.height           = 152
-$batchEditPanel.width            = 169
-$batchEditPanel.location         = New-Object System.Drawing.Point(218,13)
+$batchEditPanel.height           = 180
+$batchEditPanel.width            = 180
+$batchEditPanel.location         = New-Object System.Drawing.Point(218,14)
 $batchEditPanel.BackColor     = "#e0e0eb"
 
 $ToolTip1                        = New-Object system.Windows.Forms.ToolTip
@@ -54,7 +63,7 @@ $ToolTip1                        = New-Object system.Windows.Forms.ToolTip
 $aliasInput                      = New-Object system.Windows.Forms.TextBox
 $aliasInput.multiline            = $false
 $aliasInput.text                 = "Collection Alias"
-$aliasInput.width                = 129
+$aliasInput.width                = 130
 $aliasInput.height               = 20
 $aliasInput.location             = New-Object System.Drawing.Point(24,16)
 $aliasInput.Font                 = 'Microsoft Sans Serif,10'
@@ -63,7 +72,7 @@ $aliasInput.ForeColor            = "#000000"
 $metadataInput                   = New-Object system.Windows.Forms.TextBox
 $metadataInput.multiline         = $false
 $metadataInput.text              = "Metadata CSV"
-$metadataInput.width             = 129
+$metadataInput.width             = 130
 $metadataInput.height            = 20
 $metadataInput.location          = New-Object System.Drawing.Point(25,47)
 $metadataInput.Font              = 'Microsoft Sans Serif,10'
@@ -73,16 +82,16 @@ $batchEditMetadataButton.BackColor  = "#1e407c"
 $batchEditMetadataButton.text    = "Batch Edit Metadata"
 $batchEditMetadataButton.width   = 156
 $batchEditMetadataButton.height  = 52
-$batchEditMetadataButton.location  = New-Object System.Drawing.Point(8,82)
+$batchEditMetadataButton.location  = New-Object System.Drawing.Point(10,103)
 $batchEditMetadataButton.Font    = 'Microsoft Sans Serif,10,style=Bold'
 $batchEditMetadataButton.ForeColor  = "#ffffff"
 
 $ToolTip1.SetToolTip($metadataInput,'Default value: metadata.csv')
-$createBatchPanel.controls.AddRange(@($createBatchButton,$processor,$deleteTIFF))
+$createBatchPanel.controls.AddRange(@($createBatchButton,$batchPathButton,$processor,$deleteTIFF))
 $CONTENTdmTools.controls.AddRange(@($createBatchPanel,$batchEditPanel))
 $batchEditPanel.controls.AddRange(@($aliasInput,$metadataInput,$batchEditMetadataButton))
 
-function getArguments {
+function Get-Arguments {
     If($processor.Text -contains "ImageMagick") {
         $global:imageProc = '-im'
     }
@@ -99,12 +108,47 @@ function getArguments {
     }
 }
 
+function Find-Folders {
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browse.SelectedPath = "C:\"
+    $browse.ShowNewFolderButton = $false
+    $browse.Description = "Select a directory"
+
+    $loop = $true
+    while($loop)
+    {
+        if ($browse.ShowDialog() -eq "OK")
+        {
+        $loop = $false
+
+		$global:source = $browse.SelectedPath
+
+        } else
+        {
+            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Would you like to try again or exit?", "Select a location", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
+            if($res -eq "Cancel")
+            {
+                #Ends script
+                return
+            }
+        }
+    }
+    $browse.SelectedPath
+    $browse.Dispose()
+}
+
 $createBatchButton.Add_Click({
-    getArguments
+    Get-Arguments
     $script = ".\batchLoad\batchLoadCreate.ps1"
-    $global:arguments = "$imageProc $tifProc"
-    #Write-Host "Start-Process $script $arguments"
-    Start-Process $script $arguments
+    $global:arguments = "$imageProc $tifProc -source " + '"' + $source+ '"'
+    Write-Host "Start-Process $script $arguments"
+    #Start-Process $script $arguments
+ })
+
+ $batchPathButton.Add_Click({
+     Find-Folders
  })
 
  $CONTENTdmTools.ShowDialog()
