d26c378ad0701f3b13f04364bc366671796ef4d1
Nathan Tallman <ntt7@psu.edu>
2019-08-03 14:48:15 -0400

    batchReOCR working, improve QC report

--- batchReOCR.ps1
+++ batchReOCR.ps1
@@ -32,6 +32,7 @@ $gm = "$dir\util\gm\gm.exe"
 $tesseract = "$dir\util\tesseract\tesseract.exe"
 $path = $(Resolve-Path "$path")
 $pwd = $(Get-Location).Path
+if (!(Test-Path $dir\logs)) { New-Item -ItemType Directory -Path $dir\logs | Out-Null }
 $log = ($dir + "\logs\batchReOCR_" + $collection + "_log_" + $(Get-Date -Format yyyy-MM-ddTHH-mm-ss-ffff) + ".txt")
 $csv = @()
 $i = 0
@@ -52,15 +53,10 @@ function Convert-OCR($ocrText) {
     } | Set-Content $ocrText 
 }
 
-function Test-Success {
-    if (!($success)) { }
-    if (!($LASTEXITCODE)) { $success -eq 1 }
-}
-
-Write-Output "----------------------------------------------" | Tee-Object -file $log
-Write-Output "$(Get-Timestamp) CONTENTdm Tools Batch Re-OCR Starting." | Tee-Object -file $log -Append
-Write-Output "Collection Alias: $collection" | Tee-Object -file $log -Append
-Write-Output "----------------------------------------------" | Tee-Object -file $log -Append
+Write-Output "----------------------------------------------" | Tee-Object -FilePath $log
+Write-Output "$(Get-Timestamp) CONTENTdm Tools Batch Re-OCR Starting." | Tee-Object -FilePath $log -Append
+Write-Output "Collection Alias: $collection" | Tee-Object -FilePath $log -Append
+Write-Output "----------------------------------------------" | Tee-Object -FilePath $log -Append
 
 # Get the CONTENTdm records for the collection
 $hits = Invoke-RestMethod "$server/dmwebservices/index.php?q=dmQuery/$collection/0/dmrecord!find/nosort/1024/1/0/0/0/0/1/0/json"
@@ -84,59 +80,70 @@ foreach ($record in $records) {
     }
 }
 if (!(Test-Path $path\tmp)) { New-Item -ItemType Directory -Path $path\tmp | Out-Null }
-$pages2ocr.GetEnumerator() | Select-Object -Property Name, Value | Export-CSV $path\tmp\items.csv -NoTypeInformation
-Get-Content $path\tmp\items.csv | Select-Object -Skip 1 | Set-Content $path\tmp\items_clean.csv
-$items = import-csv $path\tmp\items_clean.csv -Header dmnumber, file
+Set-Location $path\tmp
+$pages2ocr.GetEnumerator() | Select-Object -Property Name, Value | Export-CSV items.csv -NoTypeInformation
+Get-Content items.csv | Select-Object -Skip 1 | Set-Content items_clean.csv
+try { $items = import-csv items_clean.csv -Header dmnumber, file }
+catch {
+    Write-Warning "Error: Check your parameters and try again. Close this window at any time." | Tee-Object -FilePath $log -Append
+    Write-Output "Error: Check your parameters and try again. Close this window at any time." >>  $log
+    return
+}
 
 # Download the JP2 for each item in the collection (named for the record number, not original filename) and run Tesseract OCR
-Set-Location $path\tmp | Tee-Object -file $log -Append
 foreach ($item in $items) {
     $l++
     $imageInfo = Invoke-RestMethod ($server + "/dmwebservices/index.php?q=dmGetImageInfo/" + $collection + "/" + $item.dmnumber + "/json")
-    Write-Output ($(Get-Timestamp) + " Starting item " + $($item.dmnumber) + " (" + $l + " of " + $pages2ocr.count + " items)...") | Tee-Object -file $log -Append
+    Write-Output ($(Get-Timestamp) + " Starting item " + $($item.dmnumber) + " (" + $l + " of " + $pages2ocr.count + " items)...") | Tee-Object -FilePath $log -Append
     $imageFile = ($item.dmnumber + ".jpg")
-    Write-Output "        Downloading $imageFile." | Tee-Object -file $log -Append
-    Invoke-RestMethod ($public + "/utils/ajaxhelper/?CISOROOT=" + $collection + "&CISOPTR=" + $item.dmnumber + "&action=2&DMSCALE=100&DMWIDTH=" + $imageInfo.width + "&DMHEIGHT=" + $imageInfo.height + "&DMX=0&DMY=0") -OutFile $imageFile | Tee-Object -file $log -Append
-    Write-Output "        Optimizing image for OCR." | Tee-Object -file $log -Append
-    Invoke-Expression ("$gm mogrify -format tif -colorspace gray $imageFile") 2>&1 | Tee-Object -file $log -Append
+    Write-Output "        Downloading $imageFile." | Tee-Object -FilePath $log -Append
+    Invoke-RestMethod ($public + "/utils/ajaxhelper/?CISOROOT=" + $collection + "&CISOPTR=" + $item.dmnumber + "&action=2&DMSCALE=100&DMWIDTH=" + $imageInfo.width + "&DMHEIGHT=" + $imageInfo.height + "&DMX=0&DMY=0") -OutFile $imageFile | Tee-Object -FilePath $log -Append
+    Write-Output "        Optimizing image for OCR." | Tee-Object -FilePath $log -Append
+    Invoke-Expression ("$gm mogrify -format tif -colorspace gray $imageFile") 2>&1 | Tee-Object -FilePath $log -Append
     $imageFile = ($item.dmnumber + ".tif")
-    Write-Output "        Running Tesseract OCR on the image." | Tee-Object -file $log -Append
-    Invoke-Expression ("$tesseract $imageFile " + $item.dmnumber + " txt quiet") 2>&1 | Tee-Object -file $log -Append
+    Write-Output "        Running Tesseract OCR on the image." | Tee-Object -FilePath $log -Append
+    Invoke-Expression ("$tesseract $imageFile " + $item.dmnumber + " txt quiet") 2>&1 | Tee-Object -FilePath $log -Append
     $imageTxt = ($item.dmnumber + ".txt")
-    Write-Output "        Sanitizing OCR text for CONTENTdm." | Tee-Object -file $log -Append
+    Write-Output "        Sanitizing OCR text for CONTENTdm." | Tee-Object -FilePath $log -Append
     Convert-OCR -ocrText $imageTxt
+    if ($?) { $i++ }
     $value = (get-content $imageTxt) | Where-Object { $_.Trim(" `t") }
-    Write-Output "        Add transcript to data file for updating collection." | Tee-Object -file $log -Append
+    Write-Output "        Add transcript to data file for updating collection." | Tee-Object -FilePath $log -Append
     $itemDetails = New-Object -TypeName PSObject
     $itemDetails | Add-Member -MemberType NoteProperty -Name dmrecord -Value $item.dmnumber
     $itemDetails | Add-Member -MemberType NoteProperty -Name $field -Value $("$value")
     $csv += $itemDetails
-    $noText = ($csv | Where-Object { $_.$field -ne "" }).count
-    $csv | Where-Object { $_.$field -ne "" } | Export-CSV ocr.csv -NoTypeInformation
-    Write-Output "$(Get-Timestamp) Completed item $($item.dmnumber)." | Tee-Object -file $log -Append
-    if ($success) { $i++ }
+    Write-Output "$(Get-Timestamp) Completed item $($item.dmnumber)." | Tee-Object -FilePath $log -Append
 }
 
-# Send the new OCR to CONTENTdm
-Write-Output "$(Get-Timestamp) Sending new transcripts to CONTENTdm using batchEdit.ps1, which will also generate its own log. Errors will not appear in this log..." | Tee-Object -file $log -Append
-$command = ($dir + "\batchEdit.ps1 -csv " + $path + "\tmp\ocr.csv -collection " + $collection)
-Write-Output $command 2>&1 | Tee-Object -file $log -Append
-Try { Invoke-Expression -Command "$command" 2>&1 | Tee-Object -file $log -Append }
-Catch { Write-Host "ERROR ERROR ERROR" -Fore "red" | Tee-Object -file $log -Append ; Write-Output $Return | Tee-Object -Filepath $log -Append }
-Write-Output "$(Get-Timestamp) Transcripts sent, don't forget to re-index the collection in the Administrative GUI." | Tee-Object -file $log -Append
+# Remove items that have blank OCR output
+$noText = ($csv | Where-Object { $_.$field -eq "" }).count
+$csv | Where-Object { $_.$field -ne "" } | Export-CSV ocr.csv -NoTypeInformation
+$ocrUpdates = (Import-CSV ocr.csv).count
 
-# Cleanup the tmp files
-Write-Output "$(Get-Timestamp) Deleting any temporary files created throughout this process." | Tee-Object -file $log -Append
-Remove-Item $path\tmp -Recurse -Force | Tee-Object -file $log -Append
+# Send the new OCR to CONTENTdm
+Write-Output "$(Get-Timestamp) Sending new transcripts to CONTENTdm using batchEdit.ps1, which will also generate its own log. Errors will not appear in this log..." | Tee-Object -FilePath $log -Append
+$command = ($dir + "\batchEdit.ps1 -csv " + $path + "\tmp\ocr.csv -collection " + $collection)
+Try { Invoke-Expression -Command "$command" 2>&1 | Tee-Object -FilePath $log -Append }
+Catch { 
+    Write-Host "ERROR: Batch edit not sent to CONTENTdm Catcher." -Fore "red" | Tee-Object -FilePath $log -Append
+    Write-Output $Return | Tee-Object -FilePath $log -Append 
+}
+Write-Output "$(Get-Timestamp) Batch edit of updated transcripts complete." | Tee-Object -FilePath $log -Append
 Set-Location $pwd
 
-Write-Output "----------------------------------------------" | Tee-Object -file $log -Append
-Write-Output "$(Get-Timestamp) CONTENTdm Tools Batch Re-OCR Complete." | Tee-Object -file $log -Append
-Write-Output "Collection Alias: $collection" | Tee-Object -file $log -Append
-Write-Output "Batch Log: $log" | Tee-Object -file $log -Append
-Write-Output "Number of images in collection: $($pages2ocr.count)" | Tee-Object -file $log -Append
-Write-Output "Number of items without images: $nonImage" | Tee-Object -file $log -Append
-Write-Output "Number of items without text:   $noText" | Tee-Object -file $log -Append
-Write-Output "Number of items OCRed:          $i" | Tee-Object -file $log -Append
-Write-Output "---------------------------------------------" | Tee-Object -file $log -Append
+# Cleanup the tmp files
+Write-Output "$(Get-Timestamp) Deleting any temporary files created throughout this process." | Tee-Object -FilePath $log -Append
+Remove-Item $path\tmp -Recurse -Force | Tee-Object -FilePath $log -Append
+
+Write-Output "----------------------------------------------" | Tee-Object -FilePath $log -Append
+Write-Output "$(Get-Timestamp) CONTENTdm Tools Batch Re-OCR Complete." | Tee-Object -FilePath $log -Append
+Write-Output "Collection Alias: $collection" | Tee-Object -FilePath $log -Append
+Write-Output "Batch Log: $log" | Tee-Object -FilePath $log -Append
+Write-Output "Number of collection item with images:     $($pages2ocr.count)" | Tee-Object -FilePath $log -Append
+Write-Output "Number of collection items without images: $nonImages" | Tee-Object -FilePath $log -Append
+Write-Output "Number of items OCRed:          $i" | Tee-Object -FilePath $log -Append
+Write-Output "Number of items without text:   $noText" | Tee-Object -FilePath $log -Append
+Write-Output "Number of items updates sent:   $ocrUpdates" | Tee-Object -FilePath $log -Append
+Write-Output "---------------------------------------------" | Tee-Object -FilePath $log -Append
 Write-Host -ForegroundColor Yellow "This window can be closed at anytime. Don't forget to index the collection!"
